c921b93fa352147a2d973158aadfdec808357842
motome <sigmapinefield15@gmail.com>
2023-10-07 17:56:15 +0900

    4th commit

--- app.py
+++ app.py
@@ -44,6 +44,8 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    text = event.message.text
+
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
 
 
